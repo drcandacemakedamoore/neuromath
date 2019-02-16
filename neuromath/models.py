7e2ecc0c030b9e16489d8b60fab0498871a5136b
Makeda <makedaland@gmail.com>
2019-02-16 18:36:44 +0200

    hjk

--- neuromath/models.py
+++ neuromath/models.py
@@ -3,7 +3,77 @@
 import logging
 
 
-aboindex_2d = {
+brainindex_2d = {
+    'swatches': [
+        { 
+            'name': 'pineal',
+            'color': '61bb46',
+        },
+        {
+            'name': 'lesion',
+        },
+        {
+            'name': 'lesion',
+            'title': 'Lesion Area',
+            'color': '#fdb827',
+        },
+        {
+            'name': 'hypothalamus',
+            'title': 'Hypothalamus',
+            'color': '#61bb46',
+        },
+        {
+            'name': 'bone',
+            'title': 'Bone Area',
+            'color': '#3e3d97',
+        },
+        {
+            'name': 'fat',
+            'title': 'Fat Area',
+            'color': '#ee1f98',
+        },
+        {
+            'name': 'pineal',
+            'title': 'Pineal Area',
+            'color': '#3d7a97',
+        },
+        {
+            'name': 'kidney',
+            'title': 'Kidney Area',
+            'color': '#2804cc',
+        },
+        {
+            'name': 'psoas-muscle',
+            'title': 'Psoas Muscle',
+            'color': '#f5821f',
+        },
+        {
+            'name': 'other-muscle',
+            'title': 'Other Muscle',
+            'color': '#04b8cf',
+        },
+        {
+            'name': 'spleen',
+            'title': 'Spleen',
+            'color': '#e03a3a',
+        },
+        {
+            'name': 'other-r-o-i',
+            'title': 'Other-r-o-i',
+            'color': '#c2cf04',
+        },
+        {
+            'name': 'bowel',
+            'title': 'Bowel',
+            'color': '#963d97',
+        },
+    ],
+}
+
+
+
+
+abdoindex_2d = {
     'swatches': [
         {
             'name': 'lesion',
@@ -63,10 +133,11 @@ aboindex_2d = {
     ],
 }
 
-
 class Models:
 
     def for_page(self, page):
         logging.info('Requesting model for page: {}'.format(page))
         if page.endswith('2dabdoindex.html'):
-            return aboindex_2d
+            return abdoindex_2d
+        if page.endswith('2dbrainindex.html'):
+            return brainindex_2d
